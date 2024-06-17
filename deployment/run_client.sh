6fb464fbd76d845f23c51f63477decf56582807f
sercanka <sercanka@amazon.com>
2024-06-17 15:05:36 +0200

    clean up and refactor

--- deployment/run_client.sh
+++ deployment/run_client.sh
@@ -9,4 +9,5 @@ java -Xms16g -Xmx16g -XX:ConcGCThreads=32 -XX:+UseTransparentHugePages \
  -XX:+UseNUMA \
  -server -Dsun.rmi.dgc.server.gcInterval=0x7FFFFFFFFFFFFFFE \
  -Dsun.rmi.dgc.client.gcInterval=0x7FFFFFFFFFFFFFFE -Dfile.encoding=UTF \
+# -Dio.netty.handler.ssl.openssl.engine=qatengine \
  -jar ExchangeFlow-1.0-SNAPSHOT.jar latency-test
